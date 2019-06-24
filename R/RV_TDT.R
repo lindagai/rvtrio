08d46b721eae996ca474a9e05c8f7588adcf5418
‘Linda <lindagai@jhu.edu>
2019-06-24 18:37:23 -0400

    edited to allow user to set parameters for RV-TDT

--- R/RV_TDT.R
+++ R/RV_TDT.R
@@ -1,11 +1,30 @@
 #' Calculating RV-TDT statistic for user-defined windows
-#' @param filepath.vcf absolute filepath of vcf
-#' @param filepath.ped absolute filepath of vcf
-#' @param window.type type of window, either number of markers ("M") or width of kilobase interval ("K")
-#' @param window.size window size size of window
+#' @param plink.ped PLINK ped file
+#' @param vcf vcf file
+#' @param vcf.ped data frame containing pedigree information for the VCF
+#' @param window.type type of window, either number of markers ("M") or width of kilobase interval ("K") (doesn't work yet)
+#' @param window.size size of window in number of markers
 #' @param param parameters for RV-TDT
 
+#' @param adapt To reduce computational time, adaptive permutation is used in /rvTDT/. Every /$adapt/ permutations (default: 500 permutations), the program will check if we should keep doing permutation (which means this gene looks promising to reach the desired alpha level), or we should give up on this gene (which means this gene will not reach the desired alpha level based on the permutations we have done so far, or we have done enough permutations)
+
+#' @param alpha alpha level in adaptive permutation
+
+#' @param permut The maximum number of permutations
+
+#' @param lower_cutoff The cutoffs to determine which variants we should include in the analysis. In this example, the third column of map file is the number of minor allele counts, and here we only include the variants who have minor allele counts less than 100
+
+#' @param upper_cutoff See lower_cutoff
+
+#' @param minVariants The minimum number of variant sites for a gene. Genes with variant site number less than minVariants will be excluded from analysis (after check missing);
+
+#' @param maxMissRation The max missing ratio allowed for a variant. The variants with missing ratio greater than maxMissRatio will be excluded from analysis. In this example, we generated the genetic data file without any missing genotypes, so /--maxMissRation 1/ is used here.
+
 #' @return results data frame containing results from RV-TDT
+
+#' @importFrom dplyr left_join
+#' @importFrom varhandle unfactor
+
 
 #' @export
 #'
@@ -15,22 +34,17 @@
 #' @export
 #'
 
-#TODO: Make sure all filepaths would work on both Mac OS X and Windows
-#TODO: Add in position info for plink.ped
-#TODO: Put getting position info in its own function
-##TODO: snp.pos.df part could be made more efficient
-#TODO: tped probably shouldn't be 
-
 ########################################################
 
-RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.size=0, window.type = "M", upper = 0.2, param){
-	
+RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.size=0, window.type = "M", adapt = 500, alpha = 0.00001, permut = 2000, lower_cutoff = 0, upper_cutoff = 100, minVariants = 3, maxMissRation = 1){
+
+	    #Extract parameters
+	    parameters<-c(adapt, alpha, permut,lower_cutoff, upper_cutoff, minVariants,maxMissRation)
+
         #Obtain ped/tped and df of snp.names/positions based on file input type
-        
         if(!is.null(plink.ped)){
                 ped <-plink.ped[,1:6]
                 tped<-.getTPED(plink = plink.ped)
-                
         } else if (!is.null(vcf) & !is.null(vcf.ped)){
                 geno<-as.data.frame(geno(vcf)$GT)
                 snps<-as.data.frame(names(geno))
@@ -38,22 +52,22 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
                 snp.pos.df<-as.data.frame(cbind(names(vcf),start(rowRanges(vcf))))
                 rm(vcf)
                 colnames(snp.pos.df)<-c("snp.name","pos")
-                
+
                 #Ensure pids in PED file are in the same order as in GENO/TPED
                 names(snps)<-"pids"
-                ped<-left_join(ped,snps, by=c("pid" ="pids"))
+                ped<-dplyr::left_join(ped,snps, by=c("pid" ="pids"))
                 tped<-.getTPED(vcf.geno = geno)
-
-                
         } else {
                 print("Check your input files.")
                 return(NULL)
         }
-        
+
+        #Get input files
         map<-.getMAP(tped)
-        results<-.runRV_TDT(ped, map, tped, rv.tdt.dir, window.size, snp.pos.df, u = upper)
+
+        results<-.runRV_TDT(ped, map, tped, rv.tdt.dir, window.size, snp.pos.df, param = parameters)
         return(results)
-        
+
 }
 
 ########################################################
@@ -71,7 +85,7 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
                                       (lapply(vcf.geno,data.table::tstrsplit, "/"),
                                               recursive = FALSE)
                 )
-                tped<-sapply(tped, unfactor)
+                tped<-sapply(tped, varhandle::unfactor)
                 rownames(tped)<-rownames(vcf.geno)
         }
         return(tped)
@@ -99,7 +113,7 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
 
 ############################
 
-.runRV_TDT<-function(ped, map, tped, rv.tdt.dir, window.size=0, snp.pos.df, u, window.type  = "M"){
+.runRV_TDT<-function(ped, map, tped, rv.tdt.dir, window.size=0, snp.pos.df, param, window.type  = "M"){
 
 	    n.snps<- nrow(map)
 
@@ -121,18 +135,17 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
         #TODO: Get rid of for loop and use a list instead
         #Make this into a function, and use apply instead?
         #Indexing is very slow for data frames
-  
 
         for (i in (1:n.windows)){
         	    start.index<-i
         		end.index<-min(i+window.size-1,n.snps)
         		mid.index<- floor(start.index + (end.index - start.index)/2)
                 input.filepaths<-.getInputFilesForWindow(ped,map,tped,window.type,start.index,end.index,i)
-                curr.window.result<-.runRV_TDTOnWindow(input.filepaths,rv.tdt.dir,u)
+                curr.window.result<-.runRV_TDTOnWindow(input.filepaths,rv.tdt.dir,param)
                 pos.info<-.getWindowPos(start.index,mid.index,end.index,snp.pos.df)
                 results.df[i,1:7]<-curr.window.result
                 results.df[i,8:10]<-pos.info
-          
+
         }
 
         return(results.df)
@@ -146,44 +159,44 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
 ########################################################
 
 .getInputFilesForWindow<-function(ped, map, tped, window.type,start.index,end.index,i){
-        
+
         #TODO: fix this to get the directory functions.R is in
         data.dir<-"./"
-        
+
         file.param<-paste0("window",i,".",
                            start.index,"-",end.index,window.type)
         file.param
-        
+
         filepath.map.new<-paste0(data.dir,file.param,".map")
         filepath.tped.new<-paste0(data.dir,file.param,".tped")
         filepath.tped.new
         filepath.ped<-paste0(data.dir,"pedfile.ped")
         filepath.ped
-        
+
         sm.map<-map[start.index:end.index,]
         sm.tped<-tped[start.index:end.index,]
-        
+
         write.table(sm.map,filepath.map.new, sep="\t",col.names=FALSE,row.names = FALSE,quote = FALSE)
         write.table(sm.tped,filepath.tped.new, sep="\t",col.names=FALSE,row.names = TRUE,quote = FALSE)
-        
+
         if (i==1){
         		#Make sure ped file sample IDs are the same order as tped
         		head(ped)
                 write.table(ped,filepath.ped, sep="\t",col.names=FALSE,row.names = FALSE,quote = FALSE)
         }
-        
+
         filepaths<-c(filepath.tped.new, filepath.ped, filepath.map.new)
 }
 
 ############################
 
-.runRV_TDTOnWindow<-function(input.filepaths,rv.tdt.dir,u,adapt=100,alpha=0.00001,permut=1000){
+.runRV_TDTOnWindow<-function(input.filepaths,rv.tdt.dir,param){
 
-		#TODO: Fix u to be 0.01
-		#TODO: Add in all the parameters that RV-TDT includes
-		#TODO: Modify command accordingly
+        #TODO: Fix u to be 0.01
+        #TODO: Add in all the parameters that RV-TDT includes
+        #TODO: Modify command accordingly
 
-        .calculateRV_TDTOnWindow(input.filepaths,rv.tdt.dir, u)
+        .calculateRV_TDTOnWindow(input.filepaths,rv.tdt.dir, param)
         results<-.extract.results()
         .clean.up.rv_tdt(input.filepaths)
         return(results)
@@ -192,17 +205,31 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
 
 ############################
 
-.calculateRV_TDTOnWindow<-function(input.filepaths,rv.tdt.dir,u,adapt=100,alpha=0.00001,permut=1000){
-	
-		#TODO: Fix u to be 0.01
-		#TODO: Add in all the parameters that RV-TDT includes
-		#TODO: Modify command accordingly
+.calculateRV_TDTOnWindow<-function(input.filepaths,rv.tdt.dir,param){
+
+        #TODO: Fix u to be 0.01
+        #TODO: Add in all the parameters that RV-TDT includes
+        #TODO: Modify command accordingly
+
+        adapt<-param[1]
+
+        alpha<-param[2]
+
+        permut<-param[3]
+
+        lower_cutoff<-param[4]
+
+        upper_cutoff<-param[5]
+
+        minVariants<-param[6]
+
+        maxMissRation <-param[7]
 
         filepath.tped<-paste0("'",input.filepaths[1],"'")
         filepath.phen<-paste0("'",input.filepaths[2],"'")
         filepath.map<-paste0("'",input.filepaths[3],"'")
 		rv.tdt.dir<-paste0("'",rv.tdt.dir,"'")
-        
+
         #filepath.tped<-paste0(input.filepaths[1])
         #filepath.phen<-paste0(input.filepaths[2])
         #filepath.map<-paste0(input.filepaths[3])
@@ -220,7 +247,10 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
                         " --adapt ", adapt,
                         " --alpha ", alpha,
                         " --permut ", permut,
-                        " -u ", u
+                        " --lower_cutoff ", lower_cutoff,
+                                " --upper_cutoff ", upper_cutoff,
+                                    " --minVariants ", minVariants,
+                                    " --maxMissRation ", maxMissRation
         )
 
         #print(command)
@@ -231,16 +261,16 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
 ############################
 
 .getWindowPos<-function(start.index,mid.index,end.index,snp.pos.df){
-	
-	# Get start, end, middle position of each window 
+
+	# Get start, end, middle position of each window
 	start.pos<-as.character(snp.pos.df$pos[start.index])
 	mid.pos<-as.character(snp.pos.df$pos[start.index])
 	end.pos<-as.character(snp.pos.df$pos[end.index])
 	pos.info<-c(start.pos,mid.pos,end.pos)
-	
+
 	# Add to results df for easier graphing
 	return(pos.info)
-	
+
 }
 
 ########################################################
@@ -262,9 +292,9 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
 ############################
 
 .clean.up.rv_tdt<-function(input.filepaths){
-	
-	    #TODO: Fix getwd() to be the R package directory
-	    # Use filepath and not paste0 so it works on Windows
+
+        #TODO: Fix getwd() to be the R package directory
+        # Use filepath and not paste0 so it works on Windows
 
         #delete all input files
         filepath.tped<-paste0("'",input.filepaths[1],"'")
@@ -281,14 +311,14 @@ RV_TDT<-function(plink.ped=NULL, vcf = NULL, vcf.ped = NULL, rv.tdt.dir, window.
         #delete all output files
         currwd<-getwd()
         filepath.pval<-paste0("'",currwd,"/NA_pval/NA.pval","'")
-        filepath.results<-paste0("'",currwd,"/NA_rvTDT/NA.rvTDT","'")        
+        filepath.results<-paste0("'",currwd,"/NA_rvTDT/NA.rvTDT","'")
         delete.output.files<-paste("rm", filepath.pval, filepath.results)
         #print(delete.output.files)
         system(delete.output.files)
-        
+
         #delete all output directories
         dir.pval<-paste0("'",currwd,"/NA_pval","'")
-        dir.results<-paste0("'",currwd,"/NA_rvTDT","'")        
+        dir.results<-paste0("'",currwd,"/NA_rvTDT","'")
         delete.output.dir<-paste("rmdir", dir.pval, dir.results)
         #print(delete.output.dir)
         system(delete.output.dir)
